6bc4915a2175069dbf5fe19a189598b8aaf943b0
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2026-05-15 11:00:30 -0300

    chore: configuracao inicial do backend finalizada

---
+++ backend/src/test/java/com/desafio/membermanagement/MembermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.membermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
